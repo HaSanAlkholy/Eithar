3db5467641c4f357b3d3413f8a2888e6cecc2965
Hasan alkholy <hassan.m.h.m.a123@gmail.com.com>
2021-04-20 13:02:47 +0200

    finished

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(window).on("load", function () {
+  //preloader
+  $(".pre-loader").fadeOut("500", function () {
+    $("body").removeClass("overflow");
+    $(this).remove();
+  });
+
+  if ($(".pre-loader").length == 0) {
+    $("body").removeClass("overflow");
+  }
+
+  //nava toggle
+  $("#nava-icon").click(function (e) {
+    $("#nava").toggleClass("nava-active");
+    $("html").toggleClass("overflow");
+  });
+
+  $("#nava").click(function (e) {
+    if (
+      e.target.id == "nava" ||
+      e.target.id == "close-nava" ||
+      e.target.parentNode.id == "close-nava"
+    ) {
+      $(this).removeClass("nava-active");
+      $("html").removeClass("overflow");
+    }
+  });
+  
+
+  new Swiper("header .swiper-container", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+    updateOnWindowResize: true,
+    autoplay: {
+      delay: 5000,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  window.addEventListener("scroll", scrolled);
+
+  function scrolled() {
+    let up = document.getElementById("up");
+    if (this.scrollY > 200) {
+      up.classList.add("active-up");
+    } else {
+      up.classList.remove("active-up");
+    }
+  }
+});
